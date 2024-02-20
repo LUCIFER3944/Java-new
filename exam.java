ecc70aadd8cd716b97d03bf67a2b54487cd7ac73
Lucifer <vivekrai3944@gmail.com>
2024-02-20 08:06:07 +0530

    update

---
+++ exam.java
@@ -0,0 +1,54 @@
+// public class exam {
+
+
+// public static void lol(){
+//     int a=3;
+//     int b=4;
+//     int c=a+b;
+
+//     System.out.println(a+b); // 7
+//     return ;
+// }
+// public static void lol1(){
+//     int d=3;
+//     int e=4;
+//     int f=d+e;
+
+//     System.out.println(d+e); // 7
+//     return ;
+// }
+
+
+    
+//     public static void main(String[] args) {
+        
+//        System.out.println(lol()+lol1(););
+//     }
+// }
+public class exam {
+
+    public static int lol() {
+        int a = 3;
+        int b = 4;
+        int c = a + b;
+
+        return c;
+    }
+
+    public static int lol1() {
+        int d = 3;
+        int e = 4;
+        int f = d + e;
+
+        return f;
+    }
+
+    public static void main(String[] args) {
+        name();
+    }
+
+    public static void name() {
+        System.out.print(lol() + lol1());
+    }
+}
+
